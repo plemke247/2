b01d078d898e316951ae9718aa1ef616c9699c14
plemke247 <84066078+plemke247@users.noreply.github.com>
2021-07-09 13:37:36 +0200

    Add files via upload

---
+++ The_Poor_Jew.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="The_Poor_Jew" tilewidth="32" tileheight="32" spacing="2" margin="1" tilecount="70" columns="7">
+ <image source="The_Poor_Jew.png" trans="ff00ff" width="250" height="340"/>
+</tileset>
